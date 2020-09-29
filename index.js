70a46a72e2a9dfd5aed644555ca1067fd3ebeaff
Ramandeep Formay <ramandeepformay@gmail.com>
2020-09-29 00:19:00 -0600

    weather icon added in the dom

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const appId = "c78c6fd738c484a18569598b34605f50";
 const api = `https://api.openweathermap.org/data/2.5/weather?q=&appid=${appId}`;
 const input =document.querySelector(".location");
-const button = document.querySelector(".search-button")
-const image = document.querySelector(".icons")
-let resultData, result, description, temp, feelsLike, minTemp, maxTemp, sunrise, sunset, icon, workData;
+const button = document.querySelector(".search-button");
+const image = document.querySelector(".icons");
+const weatherCard =document.querySelector(".weatherCard");
+let resultData, result, description, temp, feelsLike, minTemp, maxTemp, sunrise, sunset, icon, filterData, htmlData,imgSrcIcon;
 
 //fetch url
 async function weatherApi(val){
@@ -15,9 +16,11 @@ async function weatherApi(val){
 
 //  Add event Listener to get the city name
 button.addEventListener("click",async (event)=>{
-     resultData=await weatherApi(input.value);
-     workData = await domManipulation(resultData);
-      console.log(workData)
+     resultData= await weatherApi(input.value);
+     filterData = domManipulation(resultData);
+      console.log(filterData)
+      
+      dataInsert(filterData)
 })
 
 // manipulate the dom
@@ -25,19 +28,54 @@ function domManipulation(domData){
     console.log(domData.weather[0].description)
     console.log(domData.main)
     console.log(image)
-    image.src="http://openweathermap.org/img/w/" + domData.weather[0].icon + ".png"
+
     description = domData.weather[0].description;
     icon=domData.weather[0].icon
     temp=domData.main.temp
     feelsLike=domData.main.feels_like
     minTemp=domData.main.temp_min
     maxTemp=domData.main.temp_max
-    sunRise=domData.sys.sunrise
-    sunSet=domData.sys.sunset
-     return [description, temp, feelsLike, minTemp, maxTemp, Sunrise, Sunset, icon]
+    sunrise=domData.sys.sunrise
+    sunset=domData.sys.sunset
+     return {description, temp, feelsLike, minTemp, maxTemp, sunrise, sunset, icon}
 }
+// data insertion
+function dataInsert(cardData){
+    console.log(cardData.icon)
+    imgSrcIcon="http://openweathermap.org/img/wn/" + filterData.icon + "@4x.png"
+    htmlData=`
+<div class="card" style="width: 18rem;">
+    <img src=${imgSrcIcon} class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <div class="d-flex justify-content-around">
+        <div class="row">
+            <div class="col-sm">
+              One of three columns
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+    </div>
+  </div>
+    </div>
+
+  </div>
+
+
+`
+weatherCard.insertAdjacentHTML("afterbegin", htmlData)
+}
+// imgSrcIcon="http://openweathermap.org/img/w/" + filterData.icon + ".png"
 
 
 
 
+
+
+// <div class="card" style="width: 18rem;">
+  
 
